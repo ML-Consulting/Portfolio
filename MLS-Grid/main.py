9ae7a91c1a0ee1d683ec4451fe4dc88a58b0d323
MAA <micah@agilityconsultinginternational.com>
2026-07-06 13:03:37 -0400

    Removed relative file path

--- MLS-Grid/main.py
+++ MLS-Grid/main.py
@@ -1,9 +1,10 @@
 # import functions_framework
-from .get_notes import write_to_bq
-from .update_writeback import call_sproc, write_df_to_sheet
-from .mls_grid import main as mls_main
+from get_notes import write_to_bq
+from update_writeback import call_sproc, write_df_to_sheet
+from mls_grid import main as mls_main
 import logging
 from config import config
+
 # @functions_framework.http
 def main() -> str:
     try:
